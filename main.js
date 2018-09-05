84af641ece7f8a0bf1a68bb09c6a3f067be8c6f0
neomie peretz <neoperetzmie@hotmail.com>
2018-09-05 12:59:38 +0300

    complete

--- main.js
+++ main.js
@@ -1,2 +1,60 @@
-//write your code here to make the tests pass
-
+class Document {
+    constructor(EmployeeName) {
+        this.EmployeeName = EmployeeName
+    }
+}
+class Employee {
+    constructor(name) {
+        this.name = name
+    }
+    work(office) {
+        for (let i = 0 ; i < 10 ; i++)
+            office.documents.push(new Document(this.name))
+    }
+}
+class Manager {
+    constructor(name) {
+        this.employees = []
+        this.name = name
+    }
+    hireEmployee(name) {
+        let employee = new Employee(name)
+        this.employees.push(employee)
+    }
+    askEmployeesToWork(office) {
+        for (let i = 0; i < this.employees.length; i++) {
+            this.employees[i].work(office)
+        }
+    }
+}
+class Cleaner {
+    constructor(name) {
+        this.name = name
+    }
+    clean() {
+        console.log("Clean")
+    }
+}
+class Office {
+    constructor() {
+        this.documents = []
+        this.managers = []
+        this.cleaners = []
+    }
+    hireCleaner(name) {
+        let cleaner = new Cleaner(name)
+        this.cleaners.push(cleaner)
+    }
+    hireManager(name) {
+        let manager = new Manager(name)
+        this.managers.push(manager)
+    }
+    startWorkDay() {
+        for (let i = 0; i < this.managers.length; i++) {
+            this.managers[i].askEmployeesToWork(this)
+        }
+        for (let i = 0; i < this.cleaners.length; i++) {
+            this.cleaners[i].clean()
+        }
+    }
+}
